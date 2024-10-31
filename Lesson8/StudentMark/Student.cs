e1ed37f5f86a4dadfa82122f2f9ec07436c7e10f
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-10-31 15:29:54 +0700

    lesson8

---
+++ Lesson8/StudentMark/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMark
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Mark { get; set; }
+        public string Gender { get; set; }  
+
+    }
+}
