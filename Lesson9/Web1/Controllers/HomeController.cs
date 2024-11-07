74a8d7e424c79528bef4d2851923e53ba5d22368
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-11-07 17:39:39 +0700

    asp

---
+++ Lesson9/Web1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web1.Models;
+
+namespace Web1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //URL: localhost:PORT/Home/Demo
+        public IActionResult Demo()
+        {
+            //View: Views/Home/Demo.cshtml
+            return View();
+        }
+
+        public IActionResult VNU()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lesson9/Web1/Controllers/VietnamController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web1.Controllers
+{
+    public class VietnamController : Controller
+    {
+        //URL: localhost:PORT/Vietnam/Index
+        //URL: localhost:PORT/Vietnam
+        public IActionResult Index()
+        {
+            //View: Views/Vietnam/Index.cshtml
+            return View();
+        }
+
+        public IActionResult Hanoi()
+        {
+            return View();
+        }
+
+        public IActionResult Danang()
+        {
+            return View();
+        }
+
+        public IActionResult HCMCity()
+        {
+            return View();
+        }
+    }
+}
